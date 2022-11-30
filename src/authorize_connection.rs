59e5577c3ada833785b35d0c7b490d0c886b610e
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2022-11-30 16:56:49 -0300

    Removed curl Command::new to make a post request

--- src/authorize_connection.rs
+++ src/authorize_connection.rs
@@ -1,5 +1,5 @@
 use hbb_common::log;
-use std::process::Command;
+use reqwest;
 
 use serde::Deserialize;
 
@@ -16,49 +16,17 @@ struct CheckLoginResp {
     success: bool,
 }
 
-pub fn post_request(url: String, body: String, header: &str) -> Result<String, std::io::Error> {
-    #[cfg(not(target_os = "linux"))]
-    {
-        let mut req = reqwest::Client::new().post(url);
-        if !header.is_empty() {
-            let tmp: Vec<&str> = header.split(": ").collect();
-            if tmp.len() == 2 {
-                req = req.header(tmp[0], tmp[1]);
-            }
+pub fn post_request(url: String, body: String, header: &str) -> Result<String, reqwest::Error> {
+    let mut req = reqwest::blocking::Client::new().post(url);
+    if !header.is_empty() {
+        let tmp: Vec<&str> = header.split(": ").collect();
+        if tmp.len() == 2 {
+            req = req.header(tmp[0], tmp[1]);
         }
-        req = req.header("Content-Type", "application/json");
-        let to = std::time::Duration::from_secs(12);
-        Ok(req.body(body).timeout(to).send().await?.text().await?)
     }
-
-    #[cfg(target_os = "linux")]
-    {
-        let mut data = vec![
-            "curl",
-            "-sS",
-            "-X",
-            "POST",
-            &url,
-            "-H",
-            "Content-Type: application/json",
-            "-d",
-            &body,
-            "--connect-timeout",
-            "12",
-        ];
-        log::error!("data request {:#?}", data);
-        if !header.is_empty() {
-            data.push("-H");
-            data.push(header);
-        }
-        let output = Command::new("curl").args(&data).output()?;
-        let res = String::from_utf8_lossy(&output.stdout).to_string();
-        if !res.is_empty() {
-            return Ok(res);
-        }
-
-        Err(std::io::Error::from(std::io::ErrorKind::ConnectionReset))
-    }
+    req = req.header("Content-Type", "application/json");
+    let to = std::time::Duration::from_secs(12);
+    Ok(req.body(body).timeout(to).send()?.text()?)
 }
 
 fn request_authorization(access_token: String) -> bool {
